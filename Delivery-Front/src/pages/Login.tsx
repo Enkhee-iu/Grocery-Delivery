ea095b585b577596248cbdcc55b1dbee8a339c0c
Enkhee-iu <enkheetuyatsetseg@gmail.com>
2026-08-21 00:00:02 +0800

    aa

--- Delivery-Front/src/pages/Login.tsx
+++ Delivery-Front/src/pages/Login.tsx
@@ -52,7 +52,29 @@ const Login = () => {
                     <label className="text-sm flex flex-col gap-1">
                       Name
                       <div className="relative ">
-                        <UserIcon className="absolute mt-3 left-3.5 top-1/2-translate-y-1/2 size-4 text-app-text-light"/>
+                        <UserIcon className="absolute mt- left-3.5 top-1/2-translate-y-1/2 size-4 text-app-text-light"/>
+                        <input 
+                        type="text"
+                        value={name}
+                        onChange={(e)=>setName(e.target.value)}
+                        required
+                        placeholder="Your name"
+                        className="w-full pl-11 pr-4 py-3 text-sm bg-white
+                        rounded-xl border bot-focus:border-app-border transition-all"
+                        />
+
+                      </div>
+                    </label>
+                   )}
+
+              </form>
+
+                  <form onSubmit={handleSubmit} className="space-y-5">
+                   {!isLoginState &&(
+                    <label className="text-sm flex flex-col gap-1">
+                      Name
+                      <div className="relative ">
+                        <UserIcon className="absolute mt- left-3.5 top-1/2-translate-y-1/2 size-4 text-app-text-light"/>
                         <input 
                         type="text"
                         value={name}
