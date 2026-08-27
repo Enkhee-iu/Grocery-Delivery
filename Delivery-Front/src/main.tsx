9871ce97abbc0e6832f3412a74eb7fd58f9d4be2
Enkhee-iu <enkheetuyatsetseg@gmail.com>
2026-08-27 14:30:12 +0800

    a

--- Delivery-Front/src/main.tsx
+++ Delivery-Front/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./views/app";
 import "./styles/index.css";
+import Hero from "./components/Home/Hero";
 
 createRoot(document.getElementById("app")!).render(
   <BrowserRouter>
